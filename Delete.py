db33897db96c0ed530369cadd0c312e3a1d3eea6
termik88 <dirkser@mail.ru>
2023-03-25 13:26:38 +0500

    edit code

--- Delete.py
+++ Delete.py
@@ -1,1 +1,22 @@
-print("Братва, не стреляте друг друга!")
+from fastapi import FastAPI
+from transformers import pipeline
+from pydantic import BaseModel
+import json
+
+
+class Item(BaseModel):
+    text: str
+
+
+app = FastAPI()
+classifier = pipeline("sentiment-analysis")   
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/predict/")
+def predict(item: Item):
+    return classifier(item.text)[0]
